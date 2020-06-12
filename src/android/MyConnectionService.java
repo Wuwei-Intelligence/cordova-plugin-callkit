fc0197793615efa6132e7b63709dd97f907580d7
Yang-tzu-yi <a298275332000@gmail.com>
2020-06-12 18:45:42 +0800

    update

--- src/android/MyConnectionService.java
+++ src/android/MyConnectionService.java
@@ -62,9 +62,8 @@ public class MyConnectionService extends ConnectionService {
             public void onReject() {
                 String sessionid = request.getExtras().getString("android_voip_session_id");
                 String reject_url = request.getExtras().getString("android_voip_reject_url");
-                if (reject_url != null && sessionid != null) {
-                    new HttpURLConnectionPost().execute("https://dev.waffle.city/intercom/v1/communities/test/reject", sessionid);
-                } 
+                new HttpURLConnectionPost().execute("https://dev.waffle.city/intercom/v1/communities/test/reject", sessionid);
+ 
                 DisconnectCause cause = new DisconnectCause(DisconnectCause.REJECTED);
                 this.setDisconnected(cause);
                 this.destroy();
