ea4cda37f7f6a441ac32443c0befe0d96eaf8714
Yang-tzu-yi <a298275332000@gmail.com>
2020-10-28 14:51:30 +0800

    update(normal): 移除tm phoneaccount

--- src/android/CordovaCall.java
+++ src/android/CordovaCall.java
@@ -96,16 +96,16 @@ public class CordovaCall extends CordovaPlugin {
         //
         tm.unregisterPhoneAccount(handle);
         //
-        if (android.os.Build.VERSION.SDK_INT >= 26) {
-            phoneAccount = new PhoneAccount.Builder(handle, appName)
-                    .setCapabilities(PhoneAccount.CAPABILITY_SELF_MANAGED).build();
-            // tm.registerPhoneAccount(phoneAccount);
-        }
-        if (android.os.Build.VERSION.SDK_INT >= 23) {
-            phoneAccount = new PhoneAccount.Builder(handle, appName)
-                    .setCapabilities(PhoneAccount.CAPABILITY_CALL_PROVIDER).build();
-            // tm.registerPhoneAccount(phoneAccount);
-        }
+        // if (android.os.Build.VERSION.SDK_INT >= 26) {
+        //     phoneAccount = new PhoneAccount.Builder(handle, appName)
+        //             .setCapabilities(PhoneAccount.CAPABILITY_SELF_MANAGED).build();
+        //     tm.registerPhoneAccount(phoneAccount);
+        // }
+        // if (android.os.Build.VERSION.SDK_INT >= 23) {
+        //     phoneAccount = new PhoneAccount.Builder(handle, appName)
+        //             .setCapabilities(PhoneAccount.CAPABILITY_CALL_PROVIDER).build();
+        //     tm.registerPhoneAccount(phoneAccount);
+        // }
         callbackContextMap.put("answer", new ArrayList<CallbackContext>());
         callbackContextMap.put("reject", new ArrayList<CallbackContext>());
         callbackContextMap.put("hangup", new ArrayList<CallbackContext>());
@@ -149,16 +149,16 @@ public class CordovaCall extends CordovaPlugin {
         } else if (action.equals("setAppName")) {
             String appName = args.getString(0);
             handle = new PhoneAccountHandle(new ComponentName(this.cordova.getActivity().getApplicationContext(),MyConnectionService.class),appName);
-            if (android.os.Build.VERSION.SDK_INT >= 26) {
-                phoneAccount = new PhoneAccount.Builder(handle, appName)
-                        .setCapabilities(PhoneAccount.CAPABILITY_SELF_MANAGED).build();
-                // tm.registerPhoneAccount(phoneAccount);
-            }
-            if (android.os.Build.VERSION.SDK_INT >= 23) {
-                phoneAccount = new PhoneAccount.Builder(handle, appName)
-                        .setCapabilities(PhoneAccount.CAPABILITY_CALL_PROVIDER).build();
-                // tm.registerPhoneAccount(phoneAccount);
-            }
+            // if (android.os.Build.VERSION.SDK_INT >= 26) {
+            //     phoneAccount = new PhoneAccount.Builder(handle, appName)
+            //             .setCapabilities(PhoneAccount.CAPABILITY_SELF_MANAGED).build();
+            //     tm.registerPhoneAccount(phoneAccount);
+            // }
+            // if (android.os.Build.VERSION.SDK_INT >= 23) {
+            //     phoneAccount = new PhoneAccount.Builder(handle, appName)
+            //             .setCapabilities(PhoneAccount.CAPABILITY_CALL_PROVIDER).build();
+            //     tm.registerPhoneAccount(phoneAccount);
+            // }
             this.callbackContext.success("App Name Changed Successfully");
             return true;
         } else if (action.equals("setIcon")) {
